8fe5030febd2961f01784a8e3e86bcad501d1dbd
François Turpin <fturpin@silamir.com>
2018-10-09 14:15:50 +0200

    Initial Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-ElectronCode
+# ElectronCode
+
+This repository contains both boilerplates and completed code.  Check out the `boilerplates` and `completed_code` dirs.
---
+++ boilerplates/tasky/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const electron = require('electron');
+
+const {app, BrowserWindow, Tray} = electron;
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+    mainWindow.loadURL('file:\\' + '\\' + __dirname + '\\src\\index.html');
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, './src/assets/' + iconName);
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        console.log(bounds.x, bounds.y);
+        // Click event bounds
+        const {x, y} = bounds;
+        // Window height and width
+        const {height, width} = mainWindow.getBounds();
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            mainWindow.setBounds({
+                x: x - width/2,
+                y: process.platform === 'win32' ? (y - height) - 10 : y,
+                height: height,
+                width: width
+            });
+            mainWindow.show();
+        }
+    })
+});
