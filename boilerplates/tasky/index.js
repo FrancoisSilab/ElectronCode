f08a6b50f30f2ba186d724bf8322a503e5d11216
François Turpin <fturpin@silamir.com>
2018-10-11 14:13:00 +0200

    Finish 4th course

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,39 +1,28 @@
 const path = require('path');
 const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
+const sprintfjs = require('sprintf-js');
 
-const {app, BrowserWindow, Tray} = electron;
+const { sprintf } = sprintfjs;
+const {app, ipcMain} = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false
-    });
-    mainWindow.loadURL('file:\\' + '\\' + __dirname + '\\src\\index.html');
+    if (process.platform === 'darwin') {
+        app.dock.hide();
+    }
+    mainWindow = new MainWindow('file:\\' + '\\' + __dirname + '\\src\\index.html');
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, './src/assets/' + iconName);
-    tray = new Tray(iconPath);
-    tray.on('click', (event, bounds) => {
-        console.log(bounds.x, bounds.y);
-        // Click event bounds
-        const {x, y} = bounds;
-        // Window height and width
-        const {height, width} = mainWindow.getBounds();
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            mainWindow.setBounds({
-                x: x - width/2,
-                y: process.platform === 'win32' ? (y - height) - 10 : y,
-                height: height,
-                width: width
-            });
-            mainWindow.show();
+    tray = new TimerTray(iconPath, mainWindow);
+    ipcMain.on('update-timer', (event, timeLeft) => {
+        if (process.platform === 'darwin') {
+            tray.setTitle(timeLeft);
+        } else if (process.platform === 'win32') {
+            tray.setToolTip(sprintf('Timer App %j', timeLeft));
         }
-    })
+    });
 });
